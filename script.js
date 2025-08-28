84cfb6ee83c8ba5d960a0fc436b7e05ce412e185
nafiz512 <nafizmuntasir512@gmail.com>
2025-08-28 19:31:44 +0600

    made call history and copy btn functionable

--- script.js
+++ script.js
@@ -5,13 +5,27 @@ function onClickFavourite(id) {
     });
 }
 
-function onClickCall(id, msg, num) {
+function onClickCall(id, msg, num, title) {
     let totalCoins = document.getElementById("coins");
     document.getElementById(id).addEventListener("click", function () {
         let coin = parseInt(totalCoins.innerText);
         if (coin >= 20) {
             alert("Calling " + msg + " : " + num);
             coin -= 20;
+            let div = document.createElement("div");
+            let item = `<div
+                            class="flex justify-between bg-gray-100 rounded-lg p-2"
+                        >
+                            <div>
+                                <h3 class="font-bold">${title}</h3>
+                                <p>${num}</p>
+                            </div>
+                            <div class="flex items-center">
+                                <h3>11:36:58 AM</h3>
+                            </div>
+                        </div>`;
+            div.innerHTML = item;
+            document.getElementById("call-history").appendChild(div);
         } else {
             alert("you need at least 20 coins");
         }
@@ -40,9 +54,10 @@ for (const card of cards) {
     onClickFavourite(favouriteId);
 
     let msg = card.children[1].children[1].innerText;
+    let title = card.children[1].children[0].innerText;
     let num = card.children[2].children[0].innerText;
     let callId = card.children[3].children[1].id;
-    onClickCall(callId, msg, num);
+    onClickCall(callId, msg, num, title);
     let copyId = card.children[3].children[0].id;
     onClickCopy(copyId, num);
 }
